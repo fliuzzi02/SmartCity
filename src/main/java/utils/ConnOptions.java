d170b5a14aa312db9d2f8992fb8972204192eff2
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2024-12-18 18:27:41 +0100

    Create Static connection options class

---
+++ src/main/java/utils/ConnOptions.java
@@ -0,0 +1,6 @@
+package main.java.utils;
+
+public class ConnOptions {
+    public static final String BROKER_ADDRESS = "tcp://tambori.dsic.upv.es:10083";
+    public static final String BASE_TOPIC = "es/upv/pros/tatami/smartcities/traffic/PTPaterna";
+}
