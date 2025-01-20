0d381408056fc3e7892998fde5936f7082a5e8e3
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2025-01-20 12:40:06 +0100

    Vehicle correctly behaves but MQTTClient crashes after publlishing

--- src/main/java/device/vehicle/Vehicle.java
+++ src/main/java/device/vehicle/Vehicle.java
@@ -4,8 +4,11 @@ import main.java.device.Device;
 import main.java.device.vehicle.navigation.components.Navigator;
 import main.java.device.vehicle.navigation.components.RoadPoint;
 import main.java.device.vehicle.navigation.components.Route;
+import main.java.device.vehicle.navigation.interfaces.IRoadPoint;
+import main.java.device.vehicle.navigation.interfaces.IRoute;
 import main.java.utils.GlobalVars;
 import main.java.utils.Logger;
+import main.java.utils.Message;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.json.JSONObject;
 
@@ -17,11 +20,14 @@ public class Vehicle extends Device {
     private Navigator navigator;
     private int speed;
 
-    Vehicle(String id, VehicleRole role, int initialSpeed, String initialSegment, int initialPosition) {
+    private String lastSegment = "";
+    private int lastPosition = -1;
+
+    Vehicle(String id, VehicleRole role, int initialSpeed, RoadPoint initialPosition) {
         super(id);
         this.role = role;
         this.navigator = new Navigator(id+"-navigator");
-        this.navigator.setCurrentPosition(new RoadPoint(initialSegment, initialPosition));
+        this.navigator.setCurrentPosition(initialPosition);
         this.speed = initialSpeed;
     }
 
@@ -35,24 +41,82 @@ public class Vehicle extends Device {
     protected void onMessage(String topic, JSONObject payload) {
         Logger.info(this.id, "Received message from " + topic + ": " + payload.toString());
         if (topic.endsWith("step")){
-            handleSimulationStep(payload);
+            handleSimulationStep(new Message(payload));
         }
     }
 
-    // TODO: This is testing
-    public void setRoute(){
-        Route route = new Route();
-        route.addRouteFragment("R1s1", 0, 29);
-        route.addRouteFragment("R1s2a", 29, 320);
-        route.addRouteFragment("R5s1", 0, 300);
+    /**
+     * This method sets the speed of the vehicle
+     * @param speed the speed of the vehicle in km/h
+     */
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
 
+    public void setRoute(IRoute route) throws RoutingException{
+        if(this.navigator.isRouting()) {
+            throw new RoutingException("Vehicle is already routing, cannot set a new route");
+        }
         this.navigator.setRoute(route);
         Logger.info(this.id, "Route set: " + route);
     }
 
-    private void handleSimulationStep(JSONObject payload){
-        if(payload.getString("type").equals("SIMULATOR_STEP")){
+    public void startRoute() throws RoutingException{
+        if(this.navigator.getRoute() == null){
+            throw new RoutingException("No route set");
+        }
+        this.navigator.startRouting();
+    }
+
+    private void handleSimulationStep(Message message){
+        // When receiving a step message, move vehicle and update position
+        if(message.getType().equals("SIMULATOR_STEP")){
             this.navigator.move(STEP_MS, speed);
+            publishUpdate(this.navigator.getCurrentPosition());
+
+            Logger.debug(this.id, "Moved to: " + this.navigator.getCurrentPosition());
+        }
+    }
+
+    private void publishUpdate(IRoadPoint position){
+        String roadSegment = position.getRoadSegment();
+        int segmentPosition = position.getPosition();
+
+        // If the vehicle is still in the same segment, just update the position
+        if(roadSegment.equals(lastSegment)){
+
+        } else {
+            // Publish vehicle entering new segment
+            // TODO: This should be a proper message
+            JSONObject msg = new JSONObject();
+            msg.put("action", "VEHICLE_IN");
+            msg.put("vehicle-role", this.role.name());
+            msg.put("vehicle-id", this.id);
+            msg.put("road-segment", roadSegment);
+            msg.put("position", segmentPosition);
+            Message message = new Message("TRAFFIC", msg);
+
+            try {
+                this.connection.publish(GlobalVars.BASE_TOPIC + "/road/" + roadSegment + "/traffic", message.toJson());
+            } catch (MqttException ignored) {
+            }
+
+            // Publish vehicle leaving previous segment
+            if(!lastSegment.equals("")){
+                msg = new JSONObject();
+                msg.put("action", "VEHICLE_OUT");
+                msg.put("vehicle-role", this.role.name());
+                msg.put("vehicle-id", this.id);
+                msg.put("road-segment", lastSegment);
+                msg.put("position", lastPosition);
+                message = new Message("TRAFFIC", msg);
+
+                try {
+                    this.connection.publish(GlobalVars.BASE_TOPIC + "/road/" + lastSegment + "/traffic", message.toJson());
+                } catch (MqttException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 
@@ -66,11 +130,31 @@ public class Vehicle extends Device {
     }
 
     public static void main(String []args){
-        Vehicle vehicle = new Vehicle("vehicle1", VehicleRole.PrivateUsage, 0, "R1s1", 0);
+        RoadPoint initialPosition = new RoadPoint("R1s2", 0);
+        Vehicle vehicle = new Vehicle("3240JVM", VehicleRole.PrivateUsage, 0, initialPosition);
         try {
             vehicle.init();
-        } catch (MqttException e) {
+
+            // Set route
+            Route route = new Route();
+            route.addRouteFragment("R1s2", 0, 29);
+            route.addRouteFragment("R1s2a", 29, 320);
+            route.addRouteFragment("R5s1", 0, 300);
+            vehicle.setRoute(route);
+
+            // Set speed
+            vehicle.setSpeed(10);
+
+            // Start route
+            vehicle.startRoute();
+        } catch (MqttException | RoutingException e) {
             Logger.error(vehicle.id, "An error occurred: " + e.getMessage());
+        }
+    }
+
+    private static class RoutingException extends Exception {
+        public RoutingException(String message) {
+            super(message);
         }
     }
 }
